dc53d63a4669f03a61ce91c7d935b4fab922720b
SATO taichi <ryushi@gmail.com>
2014-05-21 15:27:22 +0900

    purge IO::Buffer

--- ext/cool.io/CoolIoService.java
+++ ext/cool.io/CoolIoService.java
@@ -1,6 +1,5 @@
 import io.cool.Coolio;
 import io.cool.IO;
-import io.cool.IOBuffer;
 import io.cool.Loop;
 import io.cool.Socket;
 import io.cool.Watcher;
@@ -19,7 +18,6 @@ public class CoolIoService implements BasicLibraryService {
 	public boolean basicLoad(Ruby runtime) throws IOException {
 		Coolio.load(runtime);
 
-		IOBuffer.load(runtime);
 		IO.load(runtime);
 		Loop.load(runtime);
 		Watcher.load(runtime); // load servers
